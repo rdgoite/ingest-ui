ee3d1578c15695f47f80031067c4cd4fbc9a3814
Alegria Aclan <aaclan@ebi.ac.uk>
2020-02-27 14:45:08 +0000

    Put spreadsheet downloading logic in the Broker Service

--- client/src/app/shared/services/broker.service.ts
+++ client/src/app/shared/services/broker.service.ts
@@ -1,11 +1,11 @@
 import {Observable} from 'rxjs/Rx';
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
 import {catchError, tap} from 'rxjs/operators';
-import {UploadResults} from "../models/uploadResults";
+import {UploadResults} from '../models/uploadResults';
 
 import {environment} from '../../../environments/environment';
-import {throwError} from "rxjs/index";
+import {Subscription, throwError} from 'rxjs/index';
 
 // Making use of https://stackoverflow.com/questions/35326689/how-to-catch-exception-correctly-from-http-request
 
@@ -17,23 +17,8 @@ export class BrokerService {
   constructor(private http: HttpClient) {
   }
 
-  private handleError(operation: any) {
-    return (err: any) => {
-      let errMsg = `error in ${operation}()`;
-      console.log(`${errMsg}:`, err);
-
-      let httpError = {
-        message: "An error occurred in uploading spreadsheet",
-        details: `${err.name}: ${err.message}`
-      };
-
-      let error = err.error && err.error.message && err.error.details ? err.error : httpError;
-      return throwError(error);
-    }
-  }
-
-  public uploadSpreadsheet(formData, isUpdate=false): Observable<UploadResults> {
-    let uploadApiSuffix = isUpdate ? '_update': ''
+  public uploadSpreadsheet(formData, isUpdate = false): Observable<UploadResults> {
+    const uploadApiSuffix = isUpdate ? '_update' : '';
     return this.http.post<UploadResults>(`${this.API_URL}/api_upload${uploadApiSuffix}`, formData)
       .pipe(
         tap(data => console.log('server data:', data)),
@@ -41,19 +26,54 @@ export class BrokerService {
       );
   }
 
-  public downloadSpreadsheet(submissionUuid): Observable<any> {
+  public downloadSpreadsheet(submissionUuid): Subscription {
     return this.http
       .get(`${this.API_URL}/submissions/${submissionUuid}/spreadsheet`,
-        { observe: 'response',responseType: 'blob' }
+        {observe: 'response', responseType: 'blob'}
       ).map((res) => {
-        let contentDisposition = res.headers.get('content-disposition');
-        let filename = contentDisposition.split(';')[1].split('filename')[1].split('=')[1].trim();
-        let response = {
-          "data": res.body,
-          "filename": filename
-        }
-        return response
+        const contentDisposition = res.headers.get('content-disposition');
+        const filename = contentDisposition.split(';')[1].split('filename')[1].split('=')[1].trim();
+        const response = {
+          'data': res.body,
+          'filename': filename
+        };
+        return response;
+      }).subscribe(response => {
+        console.log('downloaded spreadsheet!');
+        const filename = response['filename'];
+        const newBlob = new Blob([response['data']]);
+
+        // For other browsers:
+        // Create a link pointing to the ObjectURL containing the blob.
+        const data = window.URL.createObjectURL(newBlob);
+
+        const link = document.createElement('a');
+        link.href = data;
+        link.download = filename;
+        // this is necessary as link.click() does not work on the latest firefox
+        link.dispatchEvent(new MouseEvent('click', {bubbles: true, cancelable: true, view: window}));
+
+        setTimeout(function () {
+          // For Firefox it is necessary to delay revoking the ObjectURL
+          window.URL.revokeObjectURL(data);
+          link.remove();
+        }, 100);
       });
+  }
+
+  private handleError(operation: any) {
+    return (err: any) => {
+      const errMsg = `error in ${operation}()`;
+      console.log(`${errMsg}:`, err);
+
+      const httpError = {
+        message: 'An error occurred in uploading spreadsheet',
+        details: `${err.name}: ${err.message}`
+      };
+
+      const error = err.error && err.error.message && err.error.details ? err.error : httpError;
+      return throwError(error);
+    };
   }
 
 }
--- client/src/app/submission/overview/overview.component.ts
+++ client/src/app/submission/overview/overview.component.ts
@@ -21,27 +21,7 @@ export class OverviewComponent implements OnInit {
   ngOnInit() {
   }
 
-  downloadFile() {
-    const uuid = this.submissionEnvelope['uuid']['uuid'];
-    this.brokerService.downloadSpreadsheet(uuid).subscribe(response => {
-      const filename = response['filename'];
-      const newBlob = new Blob([response['data']]);
-
-      // For other browsers:
-      // Create a link pointing to the ObjectURL containing the blob.
-      const data = window.URL.createObjectURL(newBlob);
-
-      const link = document.createElement('a');
-      link.href = data;
-      link.download = filename;
-      // this is necessary as link.click() does not work on the latest firefox
-      link.dispatchEvent(new MouseEvent('click', {bubbles: true, cancelable: true, view: window}));
-
-      setTimeout(function () {
-        // For Firefox it is necessary to delay revoking the ObjectURL
-        window.URL.revokeObjectURL(data);
-        link.remove();
-      }, 100);
-    });
+  downloadFile(uuid: string) {
+    this.brokerService.downloadSpreadsheet(uuid);
   }
 }
